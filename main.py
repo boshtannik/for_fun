d6aa7f169ddcf5784de998e287a450c734c48b6c
Jack <boshtannik@gmail.com>
2021-02-19 20:13:20 +0200

    update

--- main.py
+++ main.py
@@ -6,27 +6,59 @@ import os
 def get_sin(number, step, wide):
     return int(sin(number*step) * wide * 0.5 + wide * 0.5)
 
-ITERS_COUNT = 5
+ITERS_COUNT = 6
+NUM_WAWES = 3
+SCREEN_HEIGHT = 80
+SCREEN_WIDTH = 120
+
+SINGLE_STEP = 13
 
 iters = []
 for i in range(1, ITERS_COUNT):
     iters.append(0.1 * i)
 
+screen = [ [x for x in range(SCREEN_WIDTH)] for y in range(SCREEN_HEIGHT) ]
+
+def draw_screen(x, y):
+    global screen
+    if x > SCREEN_WIDTH or y > SCREEN_HEIGHT: return
+    screen[x][y] = 1
+
+def clear_screen():
+    global screen
+    for y in range(SCREEN_HEIGHT):
+        for x in range(SCREEN_WIDTH):
+            screen[y][x] = 0
+
+def print_screen():
+    global screen
+    for y in range(SCREEN_HEIGHT):
+        for x in range(SCREEN_WIDTH):
+            if screen[y][x] == 1:
+                print("#", end='')
+            else:
+                print(' ', end='')
+        print(end='\n')
+
+def make_iteration(got_shift):
+    for y in range(SCREEN_HEIGHT):
+        results = []
+        for iterator in iters:
+            ni = y + got_shift
+            results.append( get_sin(ni, iterator, SINGLE_STEP))
+
+        draw_screen(y, sum(results))
+
+i = 0
 while True:
-    time.sleep(0.06)
-    i = time.time()
+    time.sleep(0.05)
     try:
-        for n in range(60):
-            results = []
-            for iterator in iters:
-                ni = i + n
-                results.append( get_sin(ni, iterator, 100))
-            for width_cursor in range(max(results) + 1):
-                if width_cursor in results:
-                    print("#", end='')
-                else:
-                    print(" ", end='')
-            print('\n')
+        i+=0.1
+        clear_screen();
+        for n in range(1, NUM_WAWES + 1):
+            make_iteration(i * n * 1.618)
+        print_screen()
+
     except KeyboardInterrupt:
         break
 
